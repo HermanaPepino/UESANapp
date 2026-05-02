f1e5534dbdce2f87328ab0aa72c057313e82e4c9
Juliet T. <23101335@ue.edu.pe>
2026-05-02 13:17:48 -0500

    inicio de sesion y registro agregado

---
+++ app/src/main/java/com/example/uesanapp/presentation/home/HomeScreen.kt
@@ -0,0 +1,4 @@
+package com.example.uesanapp.presentation.home
+
+class HomeScreen {
+}
---
+++ app/src/main/java/com/example/uesanapp/presentation/navegation/AppNavGraph.kt
@@ -0,0 +1,21 @@
+package com.example.uesanapp.presentation.navegation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.uesanapp.presentation.auth.LoginScreen
+import com.example.uesanapp.presentation.auth.RegisterScreen
+import com.example.uesanapp.presentation.home.HomeScreen
+
+@Composable
+fun AppNavGraph() {
+    val navController = rememberNavController()
+
+    NavHost(navController = navController,
+            startDestination = "login") {
+        composable("login") { LoginScreen(navController) }
+        composable("register") { RegisterScreen(navController) }
+        composable("home") { HomeScreen() }
+    }
+}
